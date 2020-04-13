c884f8f7cdc80bf8e957614d3a273c85a2cdac9c
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-04-13 14:54:01 +0900

    ユーザー認証の作成
    前回からの変更点
    Routerを使用し、新しくリクエストを追加しやすいようにした
    Cors moduleをimport しブラウザからのアクセスに対応させた。
    前回作ったエンドポイントはコメントアウトし保留した。

--- index.js
+++ index.js
@@ -2,25 +2,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-//controll json from client server
+//Midleware
 app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
+app.use(cors())
+
+//routes (if you want to add request, you must add routes)
+const user_authenticationRoute = require('./routes/user_authentication');
+app.use('/user_authentication', user_authenticationRoute);
 
 // setting port (=7700)
 const port = process.env.PORT || 7700;
 
+//===================================================================
 //reading files
-const queries = require('./queries');
+//const queries = require('./queries');
 
 //setting routes
-app.post('/user_authentication/', queries.user_authenitcation);
-app.get('/get_item/', queries.getItem);
-app.post('/post_item/', queries.postItem);
+//app.post('/user_authentication/', queries.user_authenitcation);
+//app.get('/get_item/', queries.getItem);
+//app.post('/post_item/', queries.postItem);
+//===================================================================
 
 //inform the port number in console
 app.listen(port, () => console.log(`Listening on port ${port}...`));
