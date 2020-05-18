0f7d1aace36d4fc631e96c74fe8fb07c4825dfe2
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-05-18 11:54:26 +0900

    リファクタリング

--- test/simple_request.test.js
+++ test/simple_request.test.js
@@ -24,17 +24,17 @@ describe('example.jsのリクエストのテスト', () => {
         it('ログイン',async ()=>{
             const user = {"user": "user0","pass": "pass0"};
             const res = await (await request(server).post('/user_authentication').send(user));
-            expect(res.status).toBe(200)
-        })
+            expect(res.status).toBe(200);
+        });
 
         it('データ取得',async ()=>{
             const res = await request(server)
             .get('/example')
-            .set('Authorization', `Bearer ${token}`)
-            last_record_id = res.body[res.body.length-1].id // save the last record id
-            expect(res.status).toBe(200)
+            .set('Authorization', `Bearer ${token}`);
+            last_record_id = res.body[res.body.length-1].id; // save the last record id
+            expect(res.status).toBe(200);
             expect(res.type).toBe('application/json');
-        })
+        });
 
         it('データの送信',async ()=>{
             const post_json = {"text": "test_text"};
@@ -42,15 +42,15 @@ describe('example.jsのリクエストのテスト', () => {
             .post('/example')
             .set('Authorization', `Bearer ${token}`)
             .send(post_json);
-            expect(res.status).toBe(201)
-        })
+            expect(res.status).toBe(201);
+        });
 
         it('個別データの更新',async ()=>{
             const res = await request(server)
             .get(`/example/${last_record_id}`)
-            .set('Authorization', `Bearer ${token}`)
-            expect(res.status).toBe(200)
-        })
+            .set('Authorization', `Bearer ${token}`);
+            expect(res.status).toBe(200);
+        });
 
         it('データの更新',async ()=>{
             const patch_json = {"text": "test_text_patch"};
@@ -58,15 +58,15 @@ describe('example.jsのリクエストのテスト', () => {
             .patch(`/example/${last_record_id}`)
             .set('Authorization', `Bearer ${token}`)
             .send(patch_json);
-            expect(res.status).toBe(201)
-        })
+            expect(res.status).toBe(201);
+        });
 
         it('データの削除',async ()=>{
             const res = await request(server)
             .delete(`/example/${last_record_id}`)
-            .set('Authorization', `Bearer ${token}`)
-            expect(res.status).toBe(200)
-        })
+            .set('Authorization', `Bearer ${token}`);
+            expect(res.status).toBe(200);
+        });
 
     });
 });
