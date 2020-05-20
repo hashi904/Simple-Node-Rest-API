e5db60a15656c968cffe7dc25fa9430f6127d7fd
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-05-20 18:08:25 +0900

    リクエストの作成

---
+++ routes/apps_request.js
@@ -0,0 +1,199 @@
+//example query file
+//reading .env file
+require('dotenv').config();
+
+// import library
+const express = require('express');
+const router = express.Router();
+const Pool = require('pg').Pool;
+const jwt = require("jsonwebtoken");
+const log4js = require('log4js');
+const logger_config = require('../config/logger_config');
+
+//logger config level(trace, warn, error)
+log4js.configure(logger_config);
+//'system' is setting default:{appenders: ~}
+const logger = log4js.getLogger('system');
+
+//database connection setting
+const pool = new Pool({
+    user: 'root',
+    host: 'localhost',
+    database: 'wepsdb',
+    password: '',
+    port: 5432,
+});
+
+//setting schema and table
+const schema_name = "2983EF607EE549399932EE9B472B0415";
+const table_name  = "7414942E8CBC4E1E90238C15FF62BBE2";
+
+const column_001_shorttext = "001_shorttext";
+const column_002_shorttext = "002_shorttext";
+const column_003_shorttext = "003_shorttext";
+
+router.get('/', (req, res)=>{
+    // get token from header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    // token authentication
+    try{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            pool.query(`SELECT * FROM \"${schema_name}\".\"${table_name}\"`, (err, results) => {
+                if(err){
+                    logger.error(err);
+                    return res.status(400).json(err.toString());
+                }
+                res.status(200).json(results.rows);
+            });
+        });
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+router.get('/:postId', (req, res)=>{
+    // get token from header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    //get from URI
+    postId = req.params.postId;
+
+    // token authentication
+    try{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            pool.query(`SELECT * FROM \"${schema_name}\".\"${table_name}\" WHERE "ID" = \'${postId}\'`, (err, results) => {
+                if(err){
+                    logger.error(err);
+                    return res.status(400).json(err.toString());
+                }
+                res.status(200).json(results.rows);
+            });
+        });
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+router.post('/', (req, res) => {
+    // ID automatically creates using hexadecimal, and consists of 32 texts.
+    const original_text = 'ABCDEF0123456789';
+    let id = '';
+    for (let i = 0; i < 32; i++) {
+		id += original_text.charAt(Math.floor(Math.random() * original_text.length));
+	}
+
+    // get token from header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    //get json, example {"text1": "hogehoge", "text2": "hogehoge","text3": "hogehoge",}
+    const text1 = req.body.text1;
+    const text2 = req.body.text2;
+    const text3 = req.body.text3;
+
+    //token authentication
+    try{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            //we don't permit empty 
+            if(!text1){
+                return res.status(400).json("not empty");
+            }
+            if(!text2){
+                return res.status(400).json("not empty");
+            }
+            if(!text3){
+                return res.status(400).json("not empty");
+            }
+
+            pool.query(`INSERT INTO \"${schema_name}\".\"${table_name}\" ("ID", \"${column_001_shorttext}\", \"${column_002_shorttext}\",\"${column_003_shorttext}\") VALUES ($1, $2, $3, $4)`,[id, text1, text2, text3], (err, results) => {
+                if(err){
+                    logger.error(err);
+                    return res.status(400).json(err.toString());
+                }
+                res.sendStatus(201);
+            });
+        });
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+router.delete('/:postId', (req, res)=>{
+    // get token from header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    //get from URI
+    postId = req.params.postId;
+
+    // token authentication
+    try{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            pool.query(`DELETE FROM \"${schema_name}\".\"${table_name}\" WHERE "ID" = \'${postId}\'`, (err, results) => {
+                if(err){
+                    logger.error(err);
+                    return res.status(400).json(err.toString());
+                }
+                res.sendStatus(200);
+            });
+        });
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+router.patch('/:postId', (req, res)=>{
+    // get token from header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    //get from URI
+    postId = req.params.postId;
+
+    //get from json example {shorttext: "hogehoge"}
+    const text = req.body.text;
+
+    //token authentication
+    try{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            //we don't permit empty 
+            if(!text){
+                return res.status(400).json("not empty");
+            }
+            pool.query(`UPDATE \"${schema_name}\".\"${table_name}\" SET \"${column_001_shorttext}\" = \'${text}\' WHERE "ID" = \'${postId}\'`, (err, results) => {
+                if(err){
+                    logger.error(err);
+                    return res.status(400).json(err.toString());
+                }
+                res.sendStatus(201);
+            });
+        }); 
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+module.exports = router;
